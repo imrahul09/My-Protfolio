483b07893e06244f6244cfbadabdfb6916431bc3
Rahul Bhuniya <rahulbhuniya7276@gmail.com>
2025-12-19 10:36:56 +0530

    connect emailJs

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ window.addEventListener("load", () => {
 
 document.getElementById("year").textContent = new Date().getFullYear();
 
-const form = document.getElementById("contactForm");
-const result = document.getElementById("formResult");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = document.getElementById("name").value.trim();
-  const email = document.getElementById("email").value.trim();
-  const msg = document.getElementById("message").value.trim();
-  if (!name || !email || !msg) {
-    result.textContent = "Please complete the form.";
-    return;
-  }
-  // For now, just show a success message. Replace with real endpoint if you have one.
-  result.style.color = "#6ee7b7";
-  result.textContent = `Thanks ${name}! Your message has been recorded (local demo).`;
-  form.reset();
-});
+// const form = document.getElementById("contactForm");
+// const result = document.getElementById("formResult");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const name = document.getElementById("name").value.trim();
+//   const email = document.getElementById("email").value.trim();
+//   const msg = document.getElementById("message").value.trim();
+//   if (!name || !email || !msg) {
+//     result.textContent = "Please complete the form.";
+//     return;
+//   }
+//   // For now, just show a success message. Replace with real endpoint if you have one.
+//   result.style.color = "#6ee7b7";
+//   result.textContent = `Thanks ${name}! Your message has been recorded (local demo).`;
+//   form.reset();
+// });
 
 document.getElementById("resetBtn").addEventListener("click", () => {
   form.reset();
@@ -45,3 +45,19 @@ ScrollReveal().reveal("#projects .project", {
 });
 ScrollReveal().reveal("#skills", { origin: "right", delay: 200 });
 ScrollReveal().reveal("#contact", { origin: "bottom", delay: 200 });
+
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs
+    .sendForm("service_q0uee2i", "template_m3ej9r2", this)
+    .then(function () {
+      alert("Message sent successfully!");
+      form.reset();
+    })
+    .catch(function (error) {
+      alert("Something went wrong!");
+      console.log(error);
+    });
+});
